6c77cc3e9d5004dce69bafea9de2142c8e249c79
Daniel Berg <roosta042@gmail.com>
2015-03-17 21:20:04 +0100

    Add backup-current first draft, tasks file
    Add initial draft of backup script, lacking root and multi file support

--- backup-current.sh
+++ backup-current.sh
@@ -1,5 +1,43 @@
 #!/bin/bash
+#
+# copy file to either pwd (-w||--working-dir) or
+# backup directory in home(Backup) with hostname and directory structure of
+# source file. (~/Backup/[HOSTNAME]/[PWD]/FILE)
+# Append ISO formatted date and .bak to file (FILENAME.DATE.bak)
+#
+# TODO: Add support for multiple files
+# TODO: add root support
 
-#TODO: add options, one for ~/Backup and one for pwd
+bakpath=~/tests/$(hostname)$(pwd)
 
-cp
+# test for arguments
+
+# only filename provided run default behaviour
+if [[ $# -gt 0 && -f "$1" ]]; then
+  if [[ ! -d ${bakpath} ]]; then
+    mkdir -p "${bakpath}"
+  fi
+  cp "$1" "${bakpath}/${1}.$(date -I).bak"
+elif [[  $# -gt 0 ]]; then
+  while [[ $# ]]; do
+    case "$1" in
+      -h|--help)
+        echo "Help placeholder"
+        exit 0
+        ;;
+      -w|--working-dir)
+        shift
+        if test $# -gt 0; then
+          cp "${1}" "./${1}.bak"
+        else
+          echo "no file specified"
+          exit 1
+        fi
+        shift
+        ;;
+      *)
+        break
+        ;;
+    esac
+  done
+fi
