a37b436726ce619d7ecde1d43f7e73cfef474445
Daniel Berg <mail@roosta.sh>
2016-04-21 14:21:53 +0200

    Use recursive removal when removing driver

--- gfx-drv-swap.sh
+++ gfx-drv-swap.sh
@@ -6,8 +6,8 @@ NOUVEAU=xf86-video-nouveau
 # Replace -R with -Rs to if you want to remove the unneeded dependencies
 if [ $(pacman -Qqs ^mesa-libgl$) ]; then
     pacman -S $NVIDIA ${NVIDIA}-libgl # Add lib32-${NVIDIA}-libgl and ${NVIDIA}-lts if needed
-    # pacman -R $NOUVEAU
+    # pacman -Rs $NOUVEAU
 elif [ $(pacman -Qqs ^${NVIDIA}$) ]; then
     pacman -S --needed $NOUVEAU mesa-libgl # Add lib32-mesa-libgl if needed
-    pacman -R $NVIDIA # Add ${NVIDIA}-lts if needed
+    pacman -Rs $NVIDIA # Add ${NVIDIA}-lts if needed
 fi
