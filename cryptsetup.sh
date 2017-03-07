48f77a6a0cf3acde96ebe5d3a5cb897ed29e4519
Daniel Berg <mail@roosta.sh>
2017-03-07 15:12:50 +0100

    Add cryptsetup helper script

---
+++ cryptsetup.sh
@@ -0,0 +1,32 @@
+# $1 = device $2 = name
+open () {
+  sudo cryptsetup --type luks open $1 $2 && sudo mount -t ext4 /dev/mapper/"$2" ~/mnt/"$2"
+}
+
+close () {
+  sudo umount ~/mnt/"$1" && sudo cryptsetup close $1
+}
+
+usage() {
+	cat >&2 <<EOL
+  open:             open and mount
+  close:            umound and close
+EOL
+	exit 1
+}
+
+run () {
+  (( $# >= 1 )) || usage
+  case "$1" in
+    "open")
+      open ${@:2}
+      ;;
+    "close")
+      close ${@:2}
+      ;;
+    *)
+      usage
+  esac
+}
+
+run "${@}"
