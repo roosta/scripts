682b1586486883aae87efed78af553621207180c
Daniel Berg <mail@roosta.sh>
2017-03-12 14:49:20 +0100

    Finish crypthelper

--- crypthelper.sh
+++ crypthelper.sh
@@ -1,20 +1,29 @@
+#!/bin/bash
 # $1 = device $2 = name
-open () {
-  sudo cryptsetup --type luks open $1 $2 && sudo mount -t ext4 /dev/mapper/"$2" ~/mnt/"$2"
+_open () {
+  dest="$HOME/mnt/$2"
+  if [[ ! -d "$dest" ]]; then
+    mkdir -p "$dest"
+  fi
+  sudo cryptsetup --type luks open "$1" "$2" && sudo mount -t ext4 "/dev/mapper/$2" "$HOME/mnt/$2"
 }
 
-close () {
-  sudo umount ~/mnt/"$1" && sudo cryptsetup close $1
+_close () {
+  sudo umount "$HOME/mnt/$1" && sudo cryptsetup close "$1"
 }
 
 usage() {
 	cat >&2 <<EOL
 
-Usage: crypthelper command [DEVICE] [NAME]
+Usage: crypthelper command ARGS
 
 commands:
-  open:             open and mount
-  close:            umound and close
+  open:             open device by passing these args [DEVICE] [NAME]
+  close:            close device by passing [NAME]
+
+Example:
+  crypthelper open /dev/sdh1 my-encrypted-device-name # gets mountet in ~/mnt/[name]
+  crypthelper close my-encrypted-device-name # gets mountet in ~/mnt/[name]
 EOL
 	exit 1
 }
@@ -23,10 +32,10 @@ run () {
   (( $# >= 1 )) || usage
   case "$1" in
     "open")
-      open ${@:2}
+      _open "${@:2}"
       ;;
     "close")
-      close ${@:2}
+      _close "${@:2}"
       ;;
     *)
       usage
