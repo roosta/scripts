8cc246eb0243368e14e34b44f7e119d0cfed9716
Daniel Berg <mail@roosta.sh>
2017-07-07 18:54:09 +0200

    Include polybar helper scripts

---
+++ pkg.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+pac=$(checkupdates | wc -l)
+aur=$(cower -u | wc -l)
+
+check=$((pac + aur))
+if [[ "$check" != "0" ]]; then
+  echo "$pac/$aur"
+else
+  echo "$pac/$aur"
+fi
+
+# spark $pac $aur 
