19bef53597b882d65a794c840486e7fc4e172398
Daniel Berg <mail@roosta.sh>
2017-07-10 14:42:33 +0200

    Fix a bunch of linting errors

--- ddns_start.sh
+++ ddns_start.sh
@@ -19,13 +19,12 @@ USERNAME="yourdomain.org"
 PASSWORD="password"
 HOSTNAME="host.yourdomain.org"
 
-logger -t $<basename $0) "started [$@]"
+logger -t "$(basename "$0")" "started [$*]"
 
 # check if IP have changed since last run
-if [ "x$(cat /jffs/data/prev_ip.txt)" == "x$1"];
-then
+if [ "$(cat /jffs/data/prev_ip.txt)" = "$1" ]; then
   /sbin/ddns_custom_updated 1
-  logger -t $(basename $0) "ip is not changed."
+  logger -t "$(basename "$0")" "ip is not changed."
   exit 0
 fi
 
@@ -36,19 +35,18 @@ MYIP=$(curl -s  dns.uniweb.no/checkip.php | sed 's/^.*: \([^<]*\).*$/\1/')
 case $(curl -s --user $USERNAME:$PASSWORD "http://dns.uniweb.no/DynDNSServer/?hostname=$HOSTNAME&myip=$MYIP") in
   NoCHG*)
      /sbin/ddns_custom_updated 1
-     logger -t $(basename $0) "No change"
+     logger -t "$(basename "$0")" "No change"
   ;;
   IPCHG*)
     /sbin/ddns_custom_updated 1
-    logger -t $(basename $0) "Change successfull"
+    logger -t "$(basename "$0")" "Change successfull"
   ;;
   WFPW*)
     /sbin/ddns_custom_updated 0
-    logger -t $(basename $0) "Wrong password!"
+    logger -t "$(basename "$0")" "Wrong password!"
    ;;
   *)
     /sbin/ddns_custom_updated 0
-    logger -t $(basename $0) "Host Client error"
+    logger -t "$(basename "$0")" "Host Client error"
   ;;
 esac
-
