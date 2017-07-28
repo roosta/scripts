6bc464b4ef41ba9301a7531f4824b0765fbd0262
Daniel Berg <mail@roosta.sh>
2017-07-28 15:20:54 +0200

    Increate silence monitor timeout

--- tmux-update-window.sh
+++ tmux-update-window.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 if tmux has-session -t dev 2>/dev/null; then
   tmux new-window -a -n update -d  
-  tmux setw -t update monitor-silence 30 
+  tmux setw -t update monitor-silence 120 
 fi
