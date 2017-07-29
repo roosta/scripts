798bce65b5998470886f06752e230db2b6d06212
Daniel Berg <mail@roosta.sh>
2017-07-29 17:09:07 +0200

    Increase timeout, switch to window and run pacaur

--- tmux-update-window.sh
+++ tmux-update-window.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 if tmux has-session -t dev 2>/dev/null; then
-  tmux new-window -a -n update -d  
-  tmux setw -t update monitor-silence 30 
+  tmux new-window -a -n update
+  tmux setw -t update monitor-silence 300 
+  tmux send-keys -t dev 'pacaur -Syu' C-m
 fi
