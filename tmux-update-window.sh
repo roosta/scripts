7c2212f4ba6da2db436eaf7ce8109cdad6080366
Daniel Berg <mail@roosta.sh>
2018-04-29 12:53:05 +0200

    Fix tmux-update

--- tmux-update-window.sh
+++ tmux-update-window.sh
@@ -2,5 +2,5 @@
 if tmux has-session -t dev 2>/dev/null; then
   tmux new-window -a -n update
   tmux setw -t update monitor-silence 300 
-  tmux send-keys -t dev 'pacaur -Syu' C-m
+  tmux send-keys -t main 'yay -Syu' C-m
 fi
