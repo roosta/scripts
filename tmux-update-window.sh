0743030026dd9f40b17443d78f70f8425c886908
Daniel Berg <mail@roosta.sh>
2018-06-16 19:18:37 +0200

    Fix send-keys to send keys to update window

--- tmux-update-window.sh
+++ tmux-update-window.sh
@@ -2,7 +2,7 @@
 if tmux has-session -t main 2>/dev/null; then
   tmux new-window -a -n update
   tmux setw -t update monitor-silence 300 
-  tmux send-keys -t main 'yay -Syu' C-m
+  tmux send-keys -t update 'yay -Syu' C-m
 else 
   echo "Found no session called 'main'"
   exit 1
