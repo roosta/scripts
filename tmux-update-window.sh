8d4345b9ae8ba481e0402c747de09c092c80bd7e
Daniel Berg <mail@roosta.sh>
2019-03-21 11:36:21 +0100

    Stop checking for main
    Cause issues with the work script

--- tmux-update-window.sh
+++ tmux-update-window.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-if tmux has-session -t main 2>/dev/null; then
-  tmux new-window -a -n update
-  tmux setw -t update monitor-silence 300
-  tmux send-keys -t update 'yay -Syu --sudoloop' C-m
-else
-  echo "Found no session called 'main'"
-  exit 1
-fi
+tmux new-window -a -n update
+tmux setw -t update monitor-silence 300
+tmux send-keys -t update 'yay -Syu --sudoloop' C-m
