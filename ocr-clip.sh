fb3ef43efeb8f6bac9f960a8b6ad958f09dbc76d
Daniel Berg <mail@roosta.sh>
2020-05-29 13:03:22 +0200

    Remove cat pipe

--- ocr-clip.sh
+++ ocr-clip.sh
@@ -14,8 +14,9 @@ gnome-screenshot -a -f $SCR.png
 
 mogrify -modulate 100,0 -resize 400% $SCR.png
 
-tesseract $SCR.png $SCR -l nor
-cat $SCR.txt | xclip -selection clipboard
+tesseract $SCR.png $SCR -l "$1"
+
+xclip -selection clipboard < $SCR.txt
 
 echo "------------"
 cat $SCR.txt
