db963814cf965eed3bf0f17ab120c7f3558c1207
Daniel Berg <mail@roosta.sh>
2020-05-29 13:03:08 +0200

    Add strict-mode

--- ocr-clip.sh
+++ ocr-clip.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-# Dependencies: tesseract-ocr imagemagick gnome-screenshot xclip
 # Author: Mathias Bjerke <mathbje@gmail.com>
+# Dependencies: tesseract-ocr imagemagick gnome-screenshot xclip
+# ====================================
+# Select some text on screen, and attempt to OCR it.
+# Takes language as param, ie "./ocr-clip.sh eng"
+
+# strict mode
+set -euo pipefail
 
 SCR="/tmp/screenshot-ocr-clip"
 
