b09686b486e2148be5fe039cfcf796a091dbc26b
Daniel Berg <mail@roosta.sh>
2023-11-20 10:21:50 +0100

    Force refresh when updating

--- tmux-update-window.sh
+++ tmux-update-window.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 tmux new-window -a -n update
 tmux setw -t update monitor-silence 300
-tmux send-keys -t update 'paru -Syu --sudoloop' C-m
+tmux send-keys -t update 'paru -Syyu --sudoloop' C-m
