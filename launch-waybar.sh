7f16d1e67c49df86b2956354d49ff42b23018596
Daniel Berg <mail@roosta.sh>
2024-12-23 14:44:51 +0100

    Add launch-waybar script
    Reloads on config change using entr, pass -d or --debug to enable
    GTK_DEBBUG flag

---
+++ launch-waybar.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Parse command line arguments
+DEBUG_MODE=0
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -d|--debug) DEBUG_MODE=1 ;;
+        *) echo "Unknown parameter: $1"; exit 1 ;;
+    esac
+    shift
+done
+
+CONFIG_FILES=(
+    "$HOME/etc/conf/waybar/.config/waybar/config.jsonc"
+    "$HOME/etc/conf/waybar/.config/waybar/style.css"
+)
+
+# Create the command based on debug mode
+if [ $DEBUG_MODE -eq 1 ]; then
+    printf '%s\n' "${CONFIG_FILES[@]}" | entr -r env GTK_DEBUG=interactive waybar
+else
+    printf '%s\n' "${CONFIG_FILES[@]}" | entr -r waybar
+fi
