fe65ab5399c39e961aac616c4bf2a856b81d69db
Daniel Berg <mail@roosta.sh>
2024-12-31 19:41:12 +0100

    Add chrome-scaled and ffdev-scaled
    Wayland supports mixed scaling, so no need for these anymore

---
+++ chrome-scaled.sh
@@ -0,0 +1,1 @@
+/usr/bin/google-chrome-unstable --force-device-scale-factor=1 "$@" && i3-msg workspace back_and_forth
---
+++ ffdev-scaled.sh
@@ -0,0 +1,1 @@
+env GDK_DPI_SCALE=0.5 /usr/bin/firefox-developer-edition "$@" && i3-msg workspace back_and_forth
