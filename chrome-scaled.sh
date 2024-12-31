a6ee8fd043893b8e833c2ebf01d38b1b59a11c5c
Daniel Berg <mail@roosta.sh>
2025-01-01 00:08:00 +0100

    Archive scaled browser scripts

--- chrome-scaled.sh
+++
@@ -1,1 +0,0 @@
-/usr/bin/google-chrome-unstable --force-device-scale-factor=1 "$@" && i3-msg workspace back_and_forth
--- ffdev-scaled.sh
+++
@@ -1,1 +0,0 @@
-env GDK_DPI_SCALE=0.5 /usr/bin/firefox-developer-edition "$@" && i3-msg workspace back_and_forth
