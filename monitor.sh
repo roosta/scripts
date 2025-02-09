1e12ffc8af2b2a78ac2c4c0a617ce0ca7add1013
Daniel Berg <mail@roosta.sh>
2025-02-09 15:34:47 +0100

    Add description to monitor script

--- monitor.sh
+++ monitor.sh
@@ -1,1 +1,4 @@
+# Start alacritty with custom font size and btop as a command. Start it on
+# workspace 15, and go back to the previously active workspace.
+# Requires: sway, alacritty, and btop.
 swaymsg "workspace number 15; exec alacritty -o 'font.size=16' -e btop; workspace back_and_forth"
