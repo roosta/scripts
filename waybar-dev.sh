efde2275471dae0147067995f37e60e4bd17a4b1
Daniel Berg <mail@roosta.sh>
2025-04-18 13:52:38 +0200

    Fix indent

--- waybar-dev.sh
+++ waybar-dev.sh
@@ -3,21 +3,21 @@
 # Parse command line arguments
 DEBUG_MODE=0
 while [[ "$#" -gt 0 ]]; do
-    case $1 in
-        -d|--debug) DEBUG_MODE=1 ;;
-        *) echo "Unknown parameter: $1"; exit 1 ;;
-    esac
-    shift
+  case $1 in
+    -d|--debug) DEBUG_MODE=1 ;;
+    *) echo "Unknown parameter: $1"; exit 1 ;;
+  esac
+  shift
 done
 
 CONFIG_FILES=(
-    "$HOME/etc/conf/waybar/.config/waybar/config.jsonc"
-    "$HOME/etc/conf/waybar/.config/waybar/style.css"
+  "$HOME/etc/conf/waybar/.config/waybar/config.jsonc"
+  "$HOME/etc/conf/waybar/.config/waybar/style.css"
 )
 
 # Create the command based on debug mode
 if [ $DEBUG_MODE -eq 1 ]; then
-    printf '%s\n' "${CONFIG_FILES[@]}" | entr -r env GTK_DEBUG=interactive waybar
+  printf '%s\n' "${CONFIG_FILES[@]}" | entr -r env GTK_DEBUG=interactive waybar
 else
-    printf '%s\n' "${CONFIG_FILES[@]}" | entr -r waybar
+  printf '%s\n' "${CONFIG_FILES[@]}" | entr -r waybar
 fi
