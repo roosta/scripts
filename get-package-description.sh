8b98a73ca66c1587c325618ea54e5a7c3d35d109
Daniel Berg <mail@roosta.sh>
2025-04-28 12:28:47 +0200

    Add usage example to get-package-description

--- get-package-description.sh
+++ get-package-description.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 # Quckly grab a package description and put in clipboard
+# For example, here's a nvim command that will get description for whats in the
+# " register: lua vim.cmd('! ~/scripts/get-package-description.sh ' .. vim.fn.shellescape(vim.fn.getreg('"')))
 paru -Qi "$1"|rg Description|awk -F ':' '{print $2}'|wl-copy
