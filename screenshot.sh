c2dbcba8b26cd4caf67db6e143ba2fb93cbfe408
Daniel Berg <mail@roosta.sh>
2025-05-23 19:17:34 +0200

    screenshot: try satty

--- screenshot.sh
+++ screenshot.sh
@@ -1,3 +1,3 @@
 # Capture screenshot of wayland region selected by slurp, anotate with swappy
-# Requires grim, slurp, and swappy
-grim -g "$(slurp)" - | swappy -f -
+# Requires grim, slurp, and swappy/satty
+grim -g "$(slurp)" - | satty -f -
