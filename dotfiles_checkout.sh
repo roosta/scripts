ec8583323dfcc4feac9052f1c07368bf5267c489
Daniel Berg <mail@roosta.sh>
2025-05-24 19:53:22 +0200

    dotfiles/checkout: fix issues
    Several issues running this as a script, alias might not be available,
    and deprecated cmd call

--- dotfiles_checkout.sh
+++ dotfiles_checkout.sh
@@ -1,4 +1,5 @@
 # https://wiki.archlinux.org/title/Dotfiles
 mkdir -p .dotfiles-backup && \
-dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | \
-xargs -I{} mv {} .dotfiles-backup/{}
+  /usr/bin/git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME" checkout 2>&1 | \
+  grep -E "\s+\." | awk {'print $1'} | \
+  xargs -I{} mv {} .dotfiles-backup/{}
