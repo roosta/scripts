99d53c591b26a216358871874fcaaeccbd8cdedf
Daniel Berg <mail@roosta.sh>
2025-05-27 22:22:10 +0200

    switch-*: include license preamble

--- switch-display.sh
+++ switch-display.sh
@@ -1,11 +1,35 @@
 #!/bin/bash
+#
+# Copyright Daniel Berg <mail@roosta.sh>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of
+# this software and associated documentation files (the “Software”), to deal in
+# the Software without restriction, including without limitation the rights to
+# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+# of the Software, and to permit persons to whom the Software is furnished to do
+# so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 # switch-display.sh
 # =================
 # Hyprland display switcher using dynamic monitor configs, toggle between
 # monitor layouts.
-# Usage: ./switch-display.sh [desk|mirror|tv|toggle]
 #
-# Author: Daniel Berg <mail@roosta.sh>
+# Usage: ./switch-display.sh [desk|mirror|tv|toggle]
+# Make sure to create config files:
+#   $HOME/.config/hypr/monitors/desk.conf
+#   $HOME/.config/hypr/monitors/tv.conf
+#   $HOME/.config/hypr/monitors/mirror.conf
 
 CONFIG_DIR="$HOME/.config/hypr/monitors"
 CURRENT_CONFIG="$CONFIG_DIR/current.conf"
@@ -111,8 +135,8 @@ case "$1" in
     ;;
 esac
 
-# Reload hyprland config to apply changes
-log "Reloading hyprland config..."
+# Reload hyprland and waybar
+# TODO: do binary check for waybar
 reload_waybar
 hyprctl reload
 
