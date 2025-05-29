05931302bf4f515df4c3d5b3d73856b8856f7d40
Daniel Berg <mail@roosta.sh>
2025-05-29 14:48:10 +0200

    colorpicker: fix issue with rofi menu not closing
    hyprpicker will freeze the screen for picking, but rofi doesn't have
    time to close before it does that, resulting in rofi covering up what
    you potentially want to pick

--- colorpicker.sh
+++ colorpicker.sh
@@ -1,3 +1,4 @@
 # Starts a colorpicker and saves result to clipboard
-# Requires wl-clipboard
-hyprpicker -a
+# Requires wl-clipboard. Added sleep here because otherwise rofi will be open
+# when hyprpicker "freeze" the screen
+sleep .2s && hyprpicker -a -
