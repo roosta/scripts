67597226498852f4e98a31c231c57d7411b47560
Daniel Berg <mail@roosta.sh>
2025-05-29 14:47:07 +0200

    monitor: fix for hyprland
    Might expand this on occation, but currently it only runs kitty with
    btop, font-size override isn't really needed due to display layout
    changes.

--- monitor.sh
+++ monitor.sh
@@ -1,4 +1,9 @@
+# System monitor
+# Start btop in a kitty window
+# Requires: kitty & btop
+kitty btop
+
 # Start alacritty with custom font size and btop as a command. Start it on
 # workspace 15, and go back to the previously active workspace.
-# Requires: sway, alacritty, and btop.
-swaymsg "workspace number 15; exec alacritty -o 'font.size=16' -e btop; workspace back_and_forth"
+# Requires: sway, kitty, and btop.
+# swaymsg "workspace number 15; exec kitty -o 'font_size=16' btop; workspace back_and_forth"
