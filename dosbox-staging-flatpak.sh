a39363a1778ae9990a70d13d96d32aeb9189f9f2
Daniel Berg <mail@roosta.sh>
2026-07-23 14:16:55 +0200

    Add dosbox-staging-flatpak

---
+++ dosbox-staging-flatpak.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+# MIT License
+#
+# Copyright (c) 2026 Daniel Berg <mail@roosta.sh>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of
+# this software and associated documentation files (the “Software”), to deal in
+# the Software without restriction, including without limitation the rights to
+# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+# of the Software, and to permit persons to whom the Software is furnished to do
+# so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+# BEGIN_DOC
+# ### [dosbox-staging-flatpak.sh](./dosbox-staging-flatpak.sh)
+#
+# Launch dosbox-staging via flatpak, used in Lutris to launch using
+# dosbox-staging, as it needs a binary
+#
+# Requirements:
+# - https://flatpak.org/
+# - https://flathub.org/en/apps/io.github.dosbox-staging
+#
+# Usage:
+# ```sh
+# ./dosbox-staging-flatpak.sh
+# ```
+#
+# License [MIT](./LICENSES/MIT-LICENSE.txt)
+# END_DOC
+
+# shellcheck disable=SC2093
+exec flatpak run io.github.dosbox-staging "$@"
+EOF
